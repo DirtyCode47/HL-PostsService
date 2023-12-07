78d7cd5ff848086a555ea4a0c18fa4121a47f123
DirtyCode47 <alexprog47@mail.ru>
2023-12-08 08:59:28 +1000

    Added methods in repository's interface

--- PostsService/Repositories/IPostsReepository.cs
+++ PostsService/Repositories/IPostsReepository.cs
@@ -1,6 +1,14 @@
-﻿namespace PostsService.Repositories
+﻿using PostsService.Entities;
+namespace PostsService.Repositories
 {
     public interface IPostsReepository
     {
+        public Posts Add(Posts post);
+        public Posts Get(int PostCode);
+        public Posts Delete(Posts post);
+        public Posts Update(Posts post);
+        public IEnumerable<Posts> Find(string substring);
+        public IEnumerable<Posts> GetPage(Guid id);
+        public int GetMaxPage(int page_size);
     }
 }

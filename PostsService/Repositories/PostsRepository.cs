ae6e83c14582041f0629b346bb98f55a7668a981
Михаил Свинцицкий <bandirart@gmail.com>
2023-12-20 23:56:15 +1000

    Added Update, GetPage and GetAllPosts methods

--- PostsService/Repositories/PostsRepository.cs
+++ PostsService/Repositories/PostsRepository.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PostsService.Entities;
 using PostsService.Exceptions;
+using System;
 using System.Linq.Expressions;
 using static Grpc.Core.Metadata;
+
 namespace PostsService.Repositories
 {
-    public class PostsRepository //:IPostsReepository
+    public class PostsRepository : IPostsReepository
     {
         private PostsServiceDbContext _dbContext;
         public PostsRepository(PostsServiceDbContext dbContext)
@@ -17,14 +19,38 @@ namespace PostsService.Repositories
             return _dbContext.Posts.Add(post).Entity;
         }
 
+        public Posts Get(Guid id)
+        {
+            return _dbContext.Posts.Find(id);
+        }
+
         public Posts Delete(Posts post)
         {
             return _dbContext.Posts.Remove(post).Entity;
         }
 
-        public Posts Get(Guid id)
+        //НОВОЕ
+        public Posts Update(Posts post)
         {
-            return _dbContext.Posts.Find(id);
+            return _dbContext.Posts.Update(post).Entity;
+        }
+
+        public IEnumerable<Posts> Find(string substring)
+        {
+            throw new NotImplementedException();
+        }
+
+        //НОВОЕ
+        public IEnumerable<Posts> GetPage(int page, int page_size)
+        {
+            List<Posts> allPosts = GetAllPosts().ToList();
+            return allPosts.GetRange(((page - 1) * page_size), page_size).ToArray();
+        }
+
+        //НОВОЕ
+        public IEnumerable<Posts> GetAllPosts()
+        {
+            return _dbContext.Posts;
         }
 
         public bool IsAny(Expression<Func<Posts,bool>> predicate)
@@ -32,9 +58,18 @@ namespace PostsService.Repositories
             return _dbContext.Posts.Any(predicate);
         }
 
+        public int GetMaxPage(int page_size)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
         public void Complete()
         {
             _dbContext.SaveChanges();
         }
+
+   
     }
 }
--- PostsService/Services/PostsServiceImpl.cs
+++ PostsService/Services/PostsServiceImpl.cs
@@ -11,6 +11,7 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using static Grpc.Core.Metadata;
+using System;
 
 namespace PostsService.Services
 {
@@ -53,10 +54,30 @@ namespace PostsService.Services
                 }
             });
         }
+
+        //НОВОЕ
         public override Task<UpdateResponse> Update(UpdateRequest request, ServerCallContext context)
         {
-            return Task.FromResult(new UpdateResponse { });
+            Posts entity = new Posts() { Id = Guid.Parse(request.Post.Id), Code = request.Post.Code, Name = request.Post.Name, River = request.Post.River };
+            var entry = postsRepository.Update(entity);
+
+            if (entry == null) throw new RpcException(new Status(StatusCode.InvalidArgument, "Can't find record in Db with this id"));
+
+            postsRepository.Complete();
+
+            return Task.FromResult(new UpdateResponse
+            {
+                Post = new Post
+                {
+                    Id = entity.Id.ToString(),
+                    Code = entity.Code,
+                    Name = entity.Name,
+                    River = entity.River,
+                }
+            });
         }
+
+
         public override Task<GetResponse> Get(GetRequest request, ServerCallContext context)
         {
             Guid guid = Guid.Parse(request.Id);
@@ -75,10 +96,34 @@ namespace PostsService.Services
                 }
             });
         }
+
+
         public override Task<GetPageResponse> GetPage(GetPageRequest request, ServerCallContext context)
         {
-            return Task.FromResult(new GetPageResponse { });
+            uint max_page = (uint) (postsRepository.GetAllPosts().Count() / 10) + 1; //Кол-во страниц
+            var posts = postsRepository.GetPage((int)request.PageNumber, 10);
+            
+            if(!posts.Any()) throw new RpcException(new Status(StatusCode.InvalidArgument, "Can't find elements in page"));
+
+            GetPageResponse getPageResponse = new();
+
+            foreach(var post in posts) 
+            {
+                getPageResponse.Posts.Add(new Post
+                {
+                    Id = post.Id.ToString(),
+                    Name = post.Name,
+                    Code = post.Code,
+                    River=post.River
+                });
+            }
+
+            getPageResponse.PageNumber = request.PageNumber;
+            getPageResponse.MaxPageNumber = max_page;
+
+            return Task.FromResult(getPageResponse);
         }
+
         public override Task<FindResponse> Find(FindRequest request, ServerCallContext context)
         {
             return Task.FromResult(new FindResponse { });

0d55c2d5d3f1e8083321a85dfb6f3f99bf9bc05c
DirtyCode47 <alexprog47@mail.ru>
2024-02-20 14:02:21 +1000

    Скрыл порт из program.cs, добавил его в appsettings.json

--- PostsService/Program.cs
+++ PostsService/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace PostsService
 {
@@ -10,18 +11,30 @@ namespace PostsService
             var app = builder.Build();
             app.Run();
         }
+        
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.ConfigureKestrel(options =>
+                    webBuilder.ConfigureKestrel((context, options) =>
                     {
-                        options.ListenAnyIP(50052, listenOptions =>
+                        var url = context.Configuration.GetValue<string>("MyAppConfig:Kestrel:Endpoints:Http:Url");
+
+                        var uriBuilder = new UriBuilder(url);
+                        var host = uriBuilder.Host;
+                        var port = uriBuilder.Port;
+
+                        options.ListenAnyIP(port, listenOptions =>
                         {
                             listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2;
                         });
                     });
-                    webBuilder.UseStartup<Startup>(); // ”казываетс€ Startup-класс
+
+                    webBuilder.UseStartup<Startup>();
                 });
     }
 }
